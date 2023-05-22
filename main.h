4d52c2b3b271eed78bac8142c010d69011a8b403
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-05-22 19:25:02 +0000

    🔥🔥 removed conflicts

--- main.h
+++ main.h
@@ -13,37 +13,25 @@
 
 extern char **environ;
 
-<<<<<<< HEAD
-int _putchar(char c);
-=======
 /* puts.c */
 int _putchar(char c);
 void _puts(const char *str);
 
->>>>>>> f71062b04eccb0757dbd34e2e68dad6400f5399c
 void print_prompt(void);
 int parse_input(char *input, char **args);
 
 /* exec.c file */
 int check_executable(char *executable);
 void execute_command(char **argv, char **envp);
-<<<<<<< HEAD
-char *findCommandDirectory(const char *command);
-=======
 char *find_command_directory(const char *command);
 
->>>>>>> f71062b04eccb0757dbd34e2e68dad6400f5399c
 /* fork.c file */
 void fork_process(char **args, char **envp);
 
 /* run_shell.c file */
-<<<<<<< HEAD
-void run_shell(char **argv _attribute_((unused)), char **envp);
-=======
 void run_shell(char **argv __attribute__((unused)), char **envp);
 void handle_input(char *input, char **args, char **envp);
 ssize_t read_input(char **input, size_t *input_len);
->>>>>>> f71062b04eccb0757dbd34e2e68dad6400f5399c
 
 /* stings.c file */
 void _strcpy(char *dest, const char *src);
@@ -54,11 +42,7 @@ char *_strdup(const char *str);
 
 /* _getenv.c file */
 char *_getenv(const char *name);
-ssize_t _getline(char **input, size_t *size, FILE *stream)
-<<<<<<< HEAD
-=======
 
 
->>>>>>> f71062b04eccb0757dbd34e2e68dad6400f5399c
 
 #endif
